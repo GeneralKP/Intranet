516a028373d38f6dcda4b52d2ac926c2b846a6c7
Kevin Perez <Kevin.Perez@teaminternational.com>
2022-11-03 18:02:16 -0500

    Cambios a app.js

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Thanks <code>for accessing</code> to our..
+          <h1>¡¡</h1>Thanks <code>for accessing</code> to our..
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          INTRANET
-        </a>
+        INTRANET!!
       </header>
     </div>
   );
